48e33c279b2b4ab19221e2017e17666addd17dc1
Yash <kudveyash@gmail.com>
2024-11-19 19:00:49 +0530

    various sections created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/custom/Header'
+import Hero from './components/custom/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h2>Hello World</h2>
+    <Header/>
+    <Hero/>
     </>
   )
 }
---
+++ src/create-trip/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateTrip() {
+  return (
+    <div>
+      This is create Trip section
+    </div>
+  )
+}
+
+export default CreateTrip
