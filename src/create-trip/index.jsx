ccb25a04fbb7333fc1fad6a9313b038927d156cf
Yash <kudveyash@gmail.com>
2024-11-21 18:03:08 +0530

    link button

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreateTrip() {
+
+  const [input, setInput] = useState("")
+  const handleChange = (e) =>{
+    setInput(e.target.value)
+  }
   return (
-    <div>
-      This is create Trip section
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+      <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and SafarSathi will generate a customized iternary based on your preferences.</p>
+
+      <div className='mt-20'>
+        <div>
+          <h2 className='text-xl my-3 font-medium'>Enter the destination of your choice</h2>
+          {/* <GooglePlacesAutocomplete
+          apikey=''
+          /> */}
+        <input className='w-64 p-2 text-lg border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500' 
+        type="text" 
+        placeholder='Enter destination'
+        value={input}
+        onChange={handleChange}
+        />
+        </div>
+      </div>
     </div>
   )
 }
