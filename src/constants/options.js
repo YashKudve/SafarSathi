bb37d11a038487704536359a51e509485d0cac46
Yash <kudveyash@gmail.com>
2024-11-22 15:53:33 +0530

    options array

---
+++ src/constants/options.js
@@ -0,0 +1,53 @@
+export const selectTravelsList = [
+    {
+        id:1,
+        title:"Just Me",
+        desc:"Solo travel exploration",
+        icon:"✈️",
+        people:"1 person"
+    },
+    {
+        id:2,
+        title:"A Couple",
+        desc:"Two traveles in tandem",
+        icon:"🥂",
+        people:"2 people"
+    },
+    {
+        id:3,
+        title:"Family",
+        desc:"A group of fun loving adv",
+        icon:"🏠",
+        people:"3 to 5 people"
+    },
+    {
+        id:4,
+        title:"Friends",
+        desc:"A bunch of thrill-seekes",
+        icon:" 🛥️",
+        people:"5 to 10 people"
+    },
+]
+
+export const selectBudgetOPtions = [
+    {
+        id:1,
+        title:"Cheap",
+        desc:"Stay concious of cost",
+        icon:"💵",
+    },
+    {
+        id:2,
+        title:"Moderate",
+        desc:"Keep cpst on average side",
+        icon:"💰",
+    },
+    {
+        id:3,
+        title:"Luxury",
+        desc:"Don't worry about cost",
+        icon:"💸",
+    }
+]
+
+export const AI_PROMPT = 'Genarate Travel Plan for Location : {location}'
